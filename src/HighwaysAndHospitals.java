35d4f6ae9ec26ee571542ede3c0596af0836c141
adokanchi <tony.dokanchi@menloschool.org>
2024-09-11 13:17:02 -0700

    Attempt 1 Part 2; Code works except for one thing timing out. Going to try using a map.

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -21,31 +21,38 @@ public class HighwaysAndHospitals {
         // If it's cheapest to just give everyone a hospital
         if (hospitalCost <= highwayCost) {
             // One hospital in each city
-            return n * hospitalCost;
+            return (long) n * hospitalCost;
         }
 
         // One hospital per disconnected subgraph, connect the rest with roads
-        int numHospitals = 0;
-        int numHighways = 0;
+        long numHospitals = 0;
+        long numHighways = 0;
 
         boolean[] seen = new boolean[n+1];
         for (int i = 1; i <= n; i++) {
+            // If it's on a new subgraph, add a hospital
             if (!seen[i]) {
                 seen[i] = true;
                 numHospitals++;
 
-                // Do BFS on node i to find the size of the full subgraph
+                // Do BFS on node i to find the full subgraph
                 Queue<Integer> toExplore = new LinkedList<Integer>();
                 toExplore.add(i);
                 while (!toExplore.isEmpty()) {
                     int startNode = toExplore.remove();
+                    /*
+                    Note: see if running this code here is faster than checking if highway[0 or 1] has been seen below
+                    if (seen[startNode]) {
+                        continue;
+                    }
+                     */
                     for (int[] highway : cities) {
-                        if (highway[0] == startNode) {
+                        if (highway[0] == startNode && !seen[highway[1]]) {
                             toExplore.add(highway[1]);
                             seen[highway[1]] = true;
                             numHighways++;
                         }
-                        else if (highway[1] == startNode) {
+                        else if (highway[1] == startNode && !seen[highway[0]]) {
                             toExplore.add(highway[0]);
                             seen[highway[0]] = true;
                             numHighways++;

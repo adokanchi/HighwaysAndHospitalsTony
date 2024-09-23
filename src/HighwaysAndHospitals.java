463bdd79c40ffdbd6baac3aeb9093458de9171f9
adokanchi <tony.dokanchi@menloschool.org>
2024-09-22 19:00:11 -0700

    Attempt 2 P3; Comments and a bugfix

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -6,50 +6,54 @@
  * Completed by: Tony Dokanchi
  */
 public class HighwaysAndHospitals {
-    public static long cost0(int n, int hospitalCost, int highwayCost, int[][] cities) {
+    public static long cost(int n, int hospitalCost, int highwayCost, int[][] cities) {
         // If it's cheapest to just give everyone a hospital
         if (hospitalCost <= highwayCost) return (long) n * hospitalCost;
         int[] rootNodes = new int[n+1];
 
         long numSubgraphs = n;
-        // For each edge AB:
         for (int[] highway : cities) {
             int start = highway[0], target = highway[1];
 
             // Path Compression
+
+            // set startRoot to be the deepest root of start
             int startRoot = start;
             while (rootNodes[startRoot] > 0) {
                 startRoot = rootNodes[startRoot];
             }
             while (rootNodes[start] > 0) {
-                // Make start point at its base root, move start up the tree one layer
+                // Make start point at its root, move start up one layer
                 int temp = rootNodes[start];
                 rootNodes[start] = startRoot;
                 start = temp;
             }
 
+            // set targetRoot to be the deepest root of target
             int targetRoot = target;
             while (rootNodes[targetRoot] > 0) {
                 targetRoot = rootNodes[targetRoot];
             }
             while (rootNodes[target] > 0) {
+                // Make target point at its root, move target up one layer
                 int temp = rootNodes[target];
                 rootNodes[target] = targetRoot;
                 target = temp;
             }
 
-            // If A != B:
+            // Combine subgraphs if necessary
             if (start != target) {
-                // Combine graphs
-
                 int startOrder = rootNodes[start];
                 int targetOrder = rootNodes[target];
 
-                if (startOrder > targetOrder) {
+                // Invert the if statement because negatives
+                if (startOrder < targetOrder) {
+                    // Minus one because negatives
                     rootNodes[start] += targetOrder - 1;
                     rootNodes[target] = start;
                 }
                 else {
+                    // Minus one because negatives
                     rootNodes[target] += startOrder - 1;
                     rootNodes[start] = target;
                 }
@@ -60,8 +64,8 @@ public class HighwaysAndHospitals {
         return numSubgraphs * hospitalCost + (n - numSubgraphs) * highwayCost;
     }
 
-    public static long cost(int n, int hospitalCost, int highwayCost, int[][] cities) {
-        // If it's cheapest to just give everyone a hospital
+    // Union-Find without weight balancing or path compression
+    public static long cost0(int n, int hospitalCost, int highwayCost, int[][] cities) {
         if (hospitalCost <= highwayCost) return (long) n * hospitalCost;
         int[] rootNodes = new int[n+1];
         long numSubgraphs = n;
@@ -69,7 +73,6 @@ public class HighwaysAndHospitals {
             int target = highway[0], start = highway[1];
             while (rootNodes[start] != 0) start = rootNodes[start];
             while (rootNodes[target] != 0) target = rootNodes[target];
-            // Merge the subgraphs across the roots if not already part of the same subgraph
             if (start != target) {
                 rootNodes[target] = start;
                 numSubgraphs--;
